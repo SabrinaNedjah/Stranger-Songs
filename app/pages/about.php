58bf964aa6fbe13d91507bd42fe6ce54003137be
kellywagnac <kelly.wagnac@gmail.com>
2018-06-16 11:49:15 +0200

    Add page about 1.0

---
+++ app/pages/about.php
@@ -0,0 +1,33 @@
+<section id="about">
+<h2 class="about_title">A stranger things experience ?</h2>
+<div class="about_text">
+    <p>This game based on the Netflix serie Stranger Things will allow you to create your own and personal song. You will also be able to publish it or share it with your friends !</p>
+</div>
+</section>
+<section id="team">
+<h2 class="team_title">Our Team</h2>
+<div class="personOne">
+    <img src="images/about/kelly_pola.png" alt="Kelly">
+    <div class="role">
+    <p>Kelly Wagnac <br>Full Stack <br>Developer</p>
+    </div>
+</div>
+<div class="personTwo">
+    <img src="images/about/sabrina_pola.png" alt="Sabrina">
+    <div class="role">
+    <p>Sabrina Nedjah <br>Full Stack <br>Developer</p>
+    </div>
+</div>
+<div class="personThree">
+    <img src="images/about/raph_pola.png" alt="Raphael">
+    <div class="role">
+    <p>Raphael Guenassia <br> Front Developer <br> and Designer</p>
+    </div>
+</div>
+<div class="personFour">
+    <img src="images/about/pauline_pola.png" alt="Pauline">
+    <div class="role">
+    <p>Pauline Aperghis <br>Designer</p>
+    </div>
+</div>
+</section>
