a1d04227b2806b302da5cefb839373c8d23540e8
Sabrina Nedjah <snedjah78@gmail.com>
2018-06-16 18:37:16 +0200

    add css about page

--- app/pages/about.php
+++ app/pages/about.php
@@ -1,33 +1,31 @@
-<section id="about">
-<h2 class="about_title">A stranger things experience ?</h2>
-<div class="about_text">
-    <p>This game based on the Netflix serie Stranger Things will allow you to create your own and personal song. You will also be able to publish it or share it with your friends !</p>
-</div>
+<section class="about">
+    <h2 class="about_title">A Stranger Things Experience ?</h2>
+    <div class="about_text">
+        <p>This game based on the Netflix serie Stranger Things will allow you to create your own and personal song. You will also be able to publish it or share it with your friends !</p>
+    </div>
 </section>
-<section id="team">
-<h2 class="team_title">Our Team</h2>
-<div class="personOne">
-    <img src="images/about/kelly_pola.png" alt="Kelly">
-    <div class="role">
-    <p>Kelly Wagnac <br>Full Stack <br>Developer</p>
-    </div>
-</div>
-<div class="personTwo">
-    <img src="images/about/sabrina_pola.png" alt="Sabrina">
-    <div class="role">
-    <p>Sabrina Nedjah <br>Full Stack <br>Developer</p>
-    </div>
-</div>
-<div class="personThree">
-    <img src="images/about/raph_pola.png" alt="Raphael">
-    <div class="role">
-    <p>Raphael Guenassia <br> Front Developer <br> and Designer</p>
-    </div>
-</div>
-<div class="personFour">
-    <img src="images/about/pauline_pola.png" alt="Pauline">
-    <div class="role">
-    <p>Pauline Aperghis <br>Designer</p>
-    </div>
-</div>
+<section class="team">
+    <h2 class="team_title">Our Team</h2>
+    <ul class="img_team">
+        <li>
+            <a href="http://www.kellywagnac.fr/" target="_blank"><img src="images/about/kelly_pola.png" alt="Kelly"></a>
+            <p class="nom"><a href="http://www.kellywagnac.fr/" target="_blank">Kelly Wagnac</a></p> 
+            <p class="poste"> Designer UX/UI and Full Stack Developer</p>
+        </li>
+        <li>
+            <a href="http://www.sabrinanedjah.fr/" target="_blank"><img src="images/about/sabrina_pola.png" alt="Sabrina"></a>
+            <p class="nom"><a href="http://www.sabrinanedjah.fr/" target="_blank">Sabrina Nedjah</a></p>
+            <p class="poste"> Designer UX/UI and Full Stack Developer</p>
+        </li>
+        <li>
+            <a href="http://raphaelguenassia.com/" target="_blank"><img src="images/about/raph_pola.png" alt="Raphael"></a>
+            <p class="nom"><a href="http://raphaelguenassia.com/" target="_blank">Raphael Guenassia</a></p>
+            <p class="poste"> Designer UI and Front Developer </p>
+        </li>
+        <li>
+            <a href="https://www.behance.net/paulineaperghis" target="_blank"><img src="images/about/pauline_pola.png" alt="Pauline"></a>
+            <p class="nom"><a href="https://www.behance.net/paulineaperghis" target="_blank">Pauline Aperghis</a></p>
+            <p class="poste"> Designer UI and Music Composer</p>
+        </li>
+    </ul>
 </section>
--- app/partials/_menu.php
+++ app/partials/_menu.php
@@ -1,14 +1,16 @@
 <!-- HEADER AND MENU -->
 <header>
+ 
     <nav>
         <ul>
+            <li>   <a href="home"><img src="images/header/stranger-things-logo.png" alt="home logo" class="logo"></a></li>
           <li><a href="home">Home</a></li>
           <li><a href="letsplay">Game</a></li>
           <li><a href="tracklist">Tracks</a></li>
           <li><a href="about">About</a></li>
+          <li>  <a href="https://www.netflix.com/fr/" target="_blank"><img src="images/header/netflix.png" alt="netflix" class="netflix"></a><li>
         </ul>
     </nav>
-    <a href="home"><img src="images/header/stranger-things-logo.png" alt="home logo" class="logo"></a>
-    <a href="https://www.netflix.com/fr/" target="_blank"><img src="images/header/netflix.png" alt="netflix" class="netflix"></a>
+  
 </header>
 <!-- HEADER AND MENU -->
