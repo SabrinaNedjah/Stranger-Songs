19e4e97badee6de3800367238b829fda332251c9
Sabrina Nedjah <snedjah78@gmail.com>
2018-06-17 20:16:53 +0200

    add sound drumkit 🚀

--- app/partials/sequencer-sounds.php
+++ app/partials/sequencer-sounds.php
@@ -5,32 +5,53 @@ $url = parse_url($_SERVER["REQUEST_URI"]);
 // Inject different kind of audio based on type.
 switch ($url['query']) {
   case 'type=1': ?>
-    <audio class="hi-hat" src="sounds/hi-hat.mp3" data-key="65"></audio>
-    <audio class="kick" src="sounds/kick.mp3" data-key="90"></audio>
-    <audio class="open-hat" src="sounds/open-hat.mp3" data-key="69"></audio>
-    <audio class="snare" src="sounds/snare.mp3" data-key="82"></audio>
-    <audio class="open-hat" src="sounds/open-hat.mp3" data-key="84"></audio>
-    <audio class="snare" src="sounds/snare.mp3" data-key="78"></audio>
+
+  <!--DRUM -->
+    <audio class="hi-hat" src="  " data-key="65"></audio>
+    <audio class="kick" src=" " data-key="90"></audio>
+    <audio class="open-hat" src=" " data-key="69"></audio>
+    <audio class="snare" src=" " data-key="82"></audio>
+    <audio class="open-hat" src=" " data-key="84"></audio>
+
+ <!--PERCUSSION -->
+    <audio class="snare" src=" " data-key="89"></audio>  
+    <audio class="hi-hat" src=" " data-key="85"></audio>
+    <audio class="kick" src=" " data-key="73"></audio>
+    <audio class="open-hat" src=" " data-key="79"></audio>
+    <audio class="snare" src=" " data-key="80"></audio>
+
+  <!--BASS -->
+    <audio class="open-hat" src=" " data-key="81"></audio>
+    <audio class="snare" src=" " data-key="83"></audio>
+    <audio class="hi-hat" src=" " data-key="68"></audio>
+    <audio class="kick" src=" " data-key="70"></audio>
+    <audio class="open-hat" src=" " data-key="71"></audio>
+
+  <!--FX -->
+    <audio class="snare" src=" " data-key="72"></audio>
+    <audio class="open-hat" src=" " data-key="74"></audio>
+    <audio class="snare" src=" " data-key="75"></audio>  
+    <audio class="hi-hat" src=" " data-key="76"></audio>
+    <audio class="kick" src=" " data-key="77"></audio>
+
+  <!--MELODY -->
+    <audio class="open-hat" src=" " data-key="87"></audio>
+    <audio class="snare" src=" " data-key="88"></audio>
+    <audio class="open-hat" src=" " data-key="67"></audio>
+    <audio class="snare" src=" " data-key="86"></audio>
+    <audio class="open-hat" src=" " data-key="66"></audio>
+    <audio class="snare" src=" " data-key="78"></audio>
     <?php
     break;
 
     case 'type=2': ?>
-    <audio class="hi-hat" src="sounds/hi-hat.mp3" data-key="65"></audio>
-    <audio class="kick" src="sounds/kick.mp3" data-key="90"></audio>
-    <audio class="open-hat" src="sounds/open-hat.mp3" data-key="69"></audio>
-    <audio class="snare" src="sounds/snare.mp3" data-key="82"></audio>
-    <audio class="open-hat" src="sounds/open-hat.mp3" data-key="84"></audio>
-    <audio class="snare" src="sounds/snare.mp3" data-key="89"></audio>
+   
     <?php
     break;
 
     case 'type=3': ?>
-    <audio class="hi-hat" src="sounds/hi-hat.mp3" data-key="65"></audio>
-    <audio class="kick" src="sounds/kick.mp3" data-key="90"></audio>
-    <audio class="open-hat" src="sounds/open-hat.mp3" data-key="69"></audio>
-    <audio class="snare" src="sounds/snare.mp3" data-key="82"></audio>
-    <audio class="open-hat" src="sounds/open-hat.mp3" data-key="84"></audio>
-    <audio class="snare" src="sounds/snare.mp3" data-key="89"></audio>
+  
+
     <?php
     break;
 
